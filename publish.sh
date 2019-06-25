d2122c1986ebb912d63001920e44a719b95a0972
ChrisBrown46 <christopher.r.brown@aggiemail.usu.edu>
2019-06-25 15:15:48 -0600

    Update Pages

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Generate the Slides and Pages
-jupyter-nbconvert Notebooks/Index.ipynb --reveal-prefix=reveal.js
-mv Notebooks/Index.html  index.html
+# jupyter-nbconvert Notebooks/Index.ipynb --reveal-prefix=reveal.js
+# mv Notebooks/Index.html  index.html
 
 cd Notebooks
 arr=(*.ipynb)
